61f29c7b5cfaf1d02c02058e1bc7e37fba47ab16
Jeremy Gillard <jeremy.gillard@enspirit.be>
2024-08-23 09:23:53 +0200

    feat: first step to basic v1

--- src/index.mjs
+++ src/index.mjs
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import inquirer from 'inquirer';
 
 const askWord = () => {
@@ -7,8 +8,39 @@ const askWord = () => {
       name: 'englishWord',
       message: 'New word (or type \\q to quit):',
     },
+    {
+      type: 'input',
+      name: 'discoveredSentence',
+      message: 'Sentence where the word was discovered:',
+      when: (answers) => answers.englishWord.toLowerCase() !== '\\q'
+    },
+    {
+      type: 'input',
+      name: 'inventedSentence',
+      message: 'Invent a sentence with this word:',
+      when: (answers) => answers.englishWord.toLowerCase() !== '\\q'
+    }
   ]).then(answers => {
     if (answers.englishWord.toLowerCase() !== '\\q') {
+      const data = {
+        word: answers.englishWord,
+        discoveredSentence: answers.discoveredSentence,
+        inventedSentence: answers.inventedSentence
+      };
+
+      // Append the data to a JSON file
+      fs.readFile('words.json', (err, fileData) => {
+        if (err && err.code !== 'ENOENT') throw err;
+
+        const json = fileData ? JSON.parse(fileData) : [];
+        json.push(data);
+
+        fs.writeFile('words.json', JSON.stringify(json, null, 2), (err) => {
+          if (err) throw err;
+          console.log('Data saved to words.json');
+        });
+      });
+
       console.log(`You entered: ${answers.englishWord}`);
       askWord(); // Call the function again to ask another question
     } else {
